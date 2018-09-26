a29304afae0b11b9a7b94113f69ab6bf7cdae97e
tangweikun <819573105@qq.com>
2018-09-26 17:15:58 +0800

    :truck: Move some --> polyfill

---
+++ polyfill/Array/some.js
@@ -0,0 +1,22 @@
+Array.prototype._some = function(callbackfn, thisArg) {
+  'use strict'
+
+  if (this == null) {
+    throw new TypeError('this is null or not defined')
+  }
+  if (typeof callbackfn !== 'function') {
+    throw new TypeError('callbackfn is not a function')
+  }
+
+  const objThis = Object(this)
+  const len = objThis.length >>> 0
+  let k = 0
+
+  while (k < len) {
+    if (k in objThis && callbackfn.call(thisArg, objThis[k], k, objThis)) {
+      return true
+    }
+  }
+
+  return false
+}
