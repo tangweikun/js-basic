d87f09ddbef157d1d26d3a79ad690ea9880a7e52
tangweikun <819573105@qq.com>
2018-09-26 19:00:20 +0800

    :sparkles: join

---
+++ polyfill/Array/join.js
@@ -0,0 +1,11 @@
+Array.prototype._join = function(separator = ',') {
+  if (this == null) {
+    throw new TypeError('this is null or not defined')
+  }
+  return this.reduce((acc, x) => acc + separator + (x || ''))
+}
+
+console.log([1, null, undefined, [2, [3], [4, 5, ['a', [], []]]], 6].join('@'))
+console.log([1, null, undefined, [2, [3], [4, 5, ['a', [], []]]], 6]._join('@'))
+console.log([1, null, undefined, [2, [3], [4, 5, ['a', [], []]]], 6].join())
+console.log([1, null, undefined, [2, [3], [4, 5, ['a', [], []]]], 6]._join())
