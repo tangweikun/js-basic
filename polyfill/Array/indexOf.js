f381da9b97561a648cef01ad2694f1a66af08cc3
tangweikun <819573105@qq.com>
2018-09-26 15:21:12 +0800

    :truck: Move indexOf --> polyfill

---
+++ polyfill/Array/indexOf.js
@@ -0,0 +1,26 @@
+Array.prototype._indexOf = function(target) {
+  'use strict'
+
+  if (this == null) {
+    throw new TypeError('this is null or not defined')
+  }
+
+  const objThis = Object(this)
+  const len = objThis.length >>> 0
+
+  const relativeFromIndex = arguments[1] >> 0
+  const fromIndex =
+    relativeFromIndex < 0
+      ? Math.max(0, relativeFromIndex + len)
+      : relativeFromIndex
+  let k = fromIndex
+
+  while (k < len) {
+    if (k in objThis && objThis[k] === target) return k
+    k++
+  }
+
+  return -1
+}
+
+console.log([2, 1]._indexOf(1, 3))
