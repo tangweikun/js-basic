d9d01d014d88b00c4c15ae04443c28f3f9c634d2
tangweikun <819573105@qq.com>
2018-09-26 16:14:46 +0800

    :truck: Move of --> polyfill

--- Array/of/polyfill.js
+++
@@ -1,6 +0,0 @@
-Array.nativeOf = function() {
-  return Array.prototype.slice.call(arguments)
-  // return [...arguments]
-}
-
-console.log(Array.nativeOf(1, 2, 3))
---
+++ polyfill/Array/of.js
@@ -0,0 +1,12 @@
+// Array._of = function() {
+//   return Array.prototype.slice.call(arguments)
+// }
+
+Array._of = function() {
+  return [...arguments]
+}
+
+console.log(Array._of())
+console.log(Array._of(undefined))
+console.log(Array._of(undefined, 2))
+console.log(Array._of(2))
