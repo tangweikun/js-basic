be9c434ff1f76bce3d6c69c8025f5f62ecd8c775
tangweikun <819573105@qq.com>
2018-09-26 21:23:09 +0800

    :sparkles: lastIndexOf

---
+++ polyfill/Array/lastIndexOf.js
@@ -0,0 +1,28 @@
+Array.prototype._lastIndexOf = function(target) {
+  'use strict'
+
+  if (this == null) {
+    throw new TypeError('this is null or not defined')
+  }
+  const objThis = Object(this)
+  const len = objThis.length >>> 0
+  let k = len - 1
+
+  if (arguments.length > 1) {
+    const temp = arguments[1] >> 0
+    k = temp < 0 ? Math.min(k, Math.max(temp + k, 0)) : Math.min(temp, k)
+  }
+
+  while (k >= 0) {
+    if (k in objThis && objThis[k] === target) return k
+    k--
+  }
+
+  return -1
+}
+
+console.log([1, , , 3, undefined, ,]._lastIndexOf(undefined))
+console.log([1, , , 3, undefined, ,]._lastIndexOf(undefined, -8))
+console.log([1, , , 3, undefined, ,].lastIndexOf(undefined, -8))
+console.log([1, , , 3, undefined, ,]._lastIndexOf(undefined, 3))
+console.log([1, , , 3, undefined, ,].lastIndexOf(undefined, 3))
