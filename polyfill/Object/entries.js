3184c0a77060395d41807ec34718dcbed69a7875
tangweikun <819573105@qq.com>
2018-09-27 13:16:47 +0800

    :sparkles: Object.entries

---
+++ polyfill/Object/entries.js
@@ -0,0 +1,11 @@
+// HELP:
+
+Object._entries = function(obj) {
+  const ownProps = Object.keys(obj)
+  let i = ownProps.length
+  const resArray = new Array(i)
+
+  while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]]
+
+  return resArray
+}
