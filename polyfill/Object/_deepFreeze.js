82dd70573d2df6248c01a955d575054b4387a29f
tangweikun <819573105@qq.com>
2018-09-27 13:29:26 +0800

    :sparkles: deepFreeze

---
+++ polyfill/Object/_deepFreeze.js
@@ -0,0 +1,20 @@
+function deepFreeze(obj) {
+  const propNames = Object.getOwnPropertyNames(obj)
+
+  // Freeze properties before freezing self
+  for (let name of propNames) {
+    let value = obj[name]
+    obj[name] = value && typeof value === 'object' ? deepFreeze(value) : value
+  }
+
+  return Object.freeze(obj)
+}
+
+var obj2 = {
+  internal: {
+    a: null,
+  },
+}
+deepFreeze(obj2)
+obj2.internal.a = 'anotherValue' // fails silently in non-strict mode
+console.log(obj2.internal.a)
