e36640a6d4c3ad0e57c3361ecd66999aa97613a0
tangweikun <819573105@qq.com>
2018-09-27 17:08:41 +0800

    :sparkles: Object.keys

---
+++ polyfill/Object/keys.js
@@ -0,0 +1,46 @@
+// HELP:
+
+Object._keys = (function() {
+  'use strict'
+
+  const hasOwnProperty = Object.prototype.hasOwnProperty
+  const hasDontEnumBug = !{ toString: null }.propertyIsEnumerable('toString')
+  const dontEnums = [
+    'toString',
+    'toLocaleString',
+    'valueOf',
+    'hasOwnProperty',
+    'isPrototypeOf',
+    'propertyIsEnumerable',
+    'constructor',
+  ]
+  const dontEnumsLength = dontEnums.length
+
+  return function(obj) {
+    if (
+      typeof obj !== 'function' &&
+      (typeof obj !== 'object' || obj === null)
+    ) {
+      throw new TypeError('Object.keys called on non-object')
+    }
+
+    const result = []
+
+    for (const prop in obj) {
+      if (hasOwnProperty.call(obj, prop)) {
+        result.push(prop)
+      }
+    }
+
+    if (hasDontEnumBug) {
+      for (let i = 0; i < dontEnumsLength; i++) {
+        if (hasOwnProperty.call(obj, dontEnums[i])) {
+          result.push(dontEnums[i])
+        }
+      }
+    }
+    return result
+  }
+})()
+
+console.log(Object._keys([2, 3]))
