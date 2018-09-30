0cabaf1d4e2ec76ab116746267c77baad246eb80
tangweikun <819573105@qq.com>
2018-09-30 13:45:13 +0800

    :sparkles: bind && once

---
+++ lodash/Function/once.js
@@ -0,0 +1,16 @@
+const once = fn => {
+  let isCalled = false
+
+  return function(...args) {
+    if (isCalled) return
+    isCalled = true
+    return fn.apply(null, args)
+  }
+}
+
+const foo = once(function(x, y) {
+  console.log('Just run once', x, y)
+})
+foo('1', '2')
+foo('2', '3')
+foo('3', '4')
