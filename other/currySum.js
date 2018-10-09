f1f428b3ec1d40b3882c13d19010c58821ef185d
tangweikun <819573105@qq.com>
2018-10-09 10:58:46 +0800

    :sparkles: currySum

---
+++ other/currySum.js
@@ -0,0 +1,17 @@
+function sum() {
+  const args = Array.prototype.slice.call(arguments)
+
+  const fn = function() {
+    const args_fn = Array.prototype.slice.call(arguments)
+    return sum.apply(null, args.concat(args_fn))
+  }
+
+  fn.valueOf = () => args.reduce((a, b) => a + b)
+
+  return fn
+}
+
+console.log(sum(1) + 0)
+console.log(sum(1, 2, 3) + 0)
+console.log(sum(1, 2)(3) + 0)
+console.log(sum(1)(2, 3)(4) + 0)
