3df8fe35d2da4b53a0d0d52d09931ae8e2f2b70e
tangweikun <819573105@qq.com>
2018-10-09 10:36:27 +0800

    :sparkles: flattenObject

---
+++ lodash/Object/flattenObject.js
@@ -0,0 +1,25 @@
+const flattenObject = (obj, prefix = '') => {
+  const res = {}
+  for (const key in obj) {
+    if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
+      Object.assign(res, flattenObject(obj[key], prefix + key + '.'))
+    } else {
+      res[prefix + key] = obj[key]
+    }
+  }
+
+  return res
+}
+
+console.log(
+  flattenObject({
+    a: { b: 1, c: 4, e: { aa: 3 } },
+    d: 44,
+    f: undefined,
+    g: null,
+    k: [],
+    h: [1, 2],
+    hh: /a/,
+    kk: {},
+  }),
+)
