b1ec89ce281db7cfbc44e4ecae90999acd0e64f6
tangweikun <819573105@qq.com>
2018-10-23 15:46:10 +0800

    :sparkles: deepFlatten by generator

---
+++ lodash/Array/deepFlatten.3.js
@@ -0,0 +1,14 @@
+const deepFlatten = arr => {
+  let res = []
+  for (let v of iteratorTree(arr)) res.push(v)
+  return res
+}
+
+function* iteratorTree(arr) {
+  for (let v of arr) {
+    if (Array.isArray(v)) yield* iteratorTree(v)
+    else yield v
+  }
+}
+
+console.log(deepFlatten([1, 2, 3, [4, [5, 6]]]))
